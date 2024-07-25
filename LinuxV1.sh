6ad14e8e828ba24d906a19f7ed2d6d24114a8aff
nicolab <nicola.bonetti@topcs.it>
2024-07-25 11:15:41 +0200

    add QuickEmu installation in applications section

--- LinuxV1.sh
+++ LinuxV1.sh
@@ -2,13 +2,16 @@
 
 # Prompt for the sudo password upfront and keep sudo alive
 sudo -v
-while true; do sudo -n true; sleep 60; done &>/dev/null &
+while true; do
+  sudo -n true
+  sleep 60
+done &>/dev/null &
 
 #--------------------------------------------------
 # Install essential terminal tools
 #--------------------------------------------------
 # Batch install essential packages with improved error handling
-sudo apt update && sudo apt upgrade -y 
+sudo apt update && sudo apt upgrade -y
 sudo apt install -y nala apt-transport-https curl cargo ca-certificates || {
   echo "Error: Failed to install essential packages. Exiting..."
   exit 1
@@ -19,6 +22,10 @@ echo "Installing command-line tools with Nala..."
 
 # Define the list of packages to install
 packages=(
+  apt-transport-https
+  curl
+  cargo
+  ca-certificates
   flameshot
   psmisc
   papirus-icon-theme
@@ -32,10 +39,11 @@ packages=(
   bat
   eza
   exa
-  tmux
   terminator
   fd-find
   nmap
+  python3
+  python3-pip
   pipx
   xsel
   zip
@@ -61,9 +69,9 @@ if [[ "$install_docker_answer" == "yes" ]]; then
   echo "2. Ubuntu distro ex: Linux Mint"
   echo "3. Debian"
   read os_choice
-  
+
   # Cleanup before Docker installation
-  for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do 
+  for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
     sudo apt-get remove -y $pkg
   done
 
@@ -76,9 +84,9 @@ if [[ "$install_docker_answer" == "yes" ]]; then
     sudo chmod a+r /etc/apt/keyrings/docker.asc
     # Add the repository to Apt sources:
     echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "$VERSION_CODENAME_CODENAME") stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME_CODENAME") stable" |
+      sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
     sudo apt-get update
     sudo nala install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   elif [[ "$os_choice" == "2" ]]; then
@@ -89,9 +97,9 @@ if [[ "$install_docker_answer" == "yes" ]]; then
     sudo chmod a+r /etc/apt/keyrings/docker.asc
     # Add the repository to Apt sources:
     echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" |
+      sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
     sudo apt-get update
     sudo nala install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   elif [[ "$os_choice" == "3" ]]; then
@@ -101,7 +109,7 @@ if [[ "$install_docker_answer" == "yes" ]]; then
     sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
     sudo apt-get update
     sudo nala install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   else
@@ -109,9 +117,9 @@ if [[ "$install_docker_answer" == "yes" ]]; then
     exit 1
   fi
 
-# Post-installation steps to use Docker as a non-root user (optional for both Ubuntu and Debian)
+  # Post-installation steps to use Docker as a non-root user (optional for both Ubuntu and Debian)
   sudo usermod -aG docker $USER
-  
+
   echo "Docker installed successfully."
 else
   echo "Docker installation skipped."
@@ -124,27 +132,27 @@ echo "Do you want to install Topgrade? (yes/no)"
 read -t 10 install_topgrade_answer || echo "Skipping Topgrade installation due to no response."
 
 if [[ "$install_topgrade_answer" == "yes" ]]; then
-    echo "Installing Topgrade..."
+  echo "Installing Topgrade..."
 
-    # Check if rustc is installed and remove it if necessary
-    if which rustc > /dev/null; then
-        echo "Rustc is installed. Removing it..."
-        sudo nala remove rustc -y
-    else
-        echo "Rustc is not installed. Proceeding..."
-    fi
+  # Check if rustc is installed and remove it if necessary
+  if which rustc >/dev/null; then
+    echo "Rustc is installed. Removing it..."
+    sudo nala remove rustc -y
+  else
+    echo "Rustc is not installed. Proceeding..."
+  fi
 
-    sudo nala install cargo -y 
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-    source $HOME/.cargo/env
-    rustup install 1.79.0
-    rustup default 1.79.0
-    cargo install topgrade
-    export PATH=$PATH:/home/$USER/.cargo/bin 
-    echo "export PATH=\$PATH:/home/\$USER/.cargo/bin" >> ~/.zshrc
-    echo "Topgrade installed successfully."
+  sudo nala install cargo -y
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+  source $HOME/.cargo/env
+  rustup install 1.79.0
+  rustup default 1.79.0
+  cargo install topgrade
+  export PATH=$PATH:/home/$USER/.cargo/bin
+  echo "export PATH=\$PATH:/home/\$USER/.cargo/bin" >>~/.zshrc
+  echo "Topgrade installed successfully."
 else
-    echo "Topgrade installation skipped."
+  echo "Topgrade installation skipped."
 fi
 
 #--------------------------------------------------
@@ -165,10 +173,10 @@ rm -rf ~/.config/nvim/.git
 
 # Install Thorium Browser
 echo "Installing Thorium Browser..."
-sudo rm -fv /etc/apt/sources.list.d/thorium.list && \
-sudo wget --no-hsts -P /etc/apt/sources.list.d/ \
-http://dl.thorium.rocks/debian/dists/stable/thorium.list && \
-sudo apt update && sudo nala install thorium-browser -y || echo "Failed to install Thorium Browser. Continuing..."
+sudo rm -fv /etc/apt/sources.list.d/thorium.list &&
+  sudo wget --no-hsts -P /etc/apt/sources.list.d/ \
+    http://dl.thorium.rocks/debian/dists/stable/thorium.list &&
+  sudo apt update && sudo nala install thorium-browser -y || echo "Failed to install Thorium Browser. Continuing..."
 
 # Install Brave Browser
 echo "Installing Brave Browser..."
@@ -182,6 +190,11 @@ echo "Installing Floorp browser"
 curl -fsSL https://ppa.ablaze.one/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
 sudo curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list 'https://ppa.ablaze.one/Floorp.list'
 sudo apt update && sudo nala install floorp -y
+
+#Install QuickEmu
+echo "Installing QuickEmu..."
+sudo apt-add-repository ppa:flexiondotorg/quickemu -y
+sudo nala install quickemu -y
 
 # Install Spotify
 echo "Installing Spotify..."
@@ -201,88 +214,86 @@ echo "Installing Zsh and Oh-My-Zsh..."
 
 OS="$(uname)"
 if [[ "$OS" == "Linux" ]] || [[ "$OS" == "Darwin" ]]; then
-    echo
+  echo
 
-    if [[ "$OS" == "Linux" ]]; then
-        echo -e "Installing zsh, bat, and git"
-        sudo nala install zsh bat git -y &> /dev/null
+  if [[ "$OS" == "Linux" ]]; then
+    echo -e "Installing zsh, bat, and git"
+    sudo nala install zsh bat git -y &>/dev/null
+  fi
+
+  if [[ "$OS" == "Darwin" ]]; then
+    echo "→ When prompted for the password, enter your Mac login password."
+    if [[ ! -e "/Library/Developer/CommandLineTools/usr/bin/git" ]]; then
+      sudo /usr/sbin/softwareupdate -i "$(softwareupdate -l |
+        grep -B 1 -E 'Command Line Tools' |
+        awk -F'*' '/^ *\\*/ {print $2}' |
+        sed -e 's/^ *Label: //' -e 's/^ *//' |
+        sort -V |
+        tail -n1)" &>/dev/null
+      sudo /usr/bin/xcode-select --switch /Library/Developer/CommandLineTools &>/dev/null
     fi
+  fi
 
-    if [[ "$OS" == "Darwin" ]]; then
-        echo "→ When prompted for the password, enter your Mac login password."
-        if [[ ! -e "/Library/Developer/CommandLineTools/usr/bin/git" ]]; then
-            sudo /usr/sbin/softwareupdate -i "$(softwareupdate -l |
-                grep -B 1 -E 'Command Line Tools' |
-                awk -F'*' '/^ *\\*/ {print $2}' |
-                sed -e 's/^ *Label: //' -e 's/^ *//' |
-                sort -V |
-                tail -n1)" &> /dev/null
-            sudo /usr/bin/xcode-select --switch /Library/Developer/CommandLineTools &> /dev/null
-        fi
-    fi
+  echo -e "Shell Configurations"
+  if [[ "$OS" == "Darwin" ]]; then
+    chsh -s /bin/zsh &>/dev/null
+  fi
+  if [[ "$OS" == "Linux" ]]; then
+    sudo usermod -s /usr/bin/zsh $(whoami) &>/dev/null
+    sudo usermod -s /usr/bin/zsh root &>/dev/null
+  fi
+  if mv -n ~/.zshrc ~/.zshrc-backup-$(date +"%Y-%m-%d") &>/dev/null; then
+    echo -e " → Backing up the current .zshrc config to .zshrc-backup-date"
+  fi
+  (cd ~/ && curl -O https://raw.githubusercontent.com/gustavohellwig/gh-zsh/main/.zshrc) &>/dev/null
+  echo "source \$HOME/.zsh/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+  echo "source \$HOME/.zsh/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh" >>~/.zshrc
+  echo "source \$HOME/.zsh/completion.zsh" >>~/.zshrc
+  echo "source \$HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >>~/.zshrc
+  echo "source \$HOME/.zsh/history.zsh" >>~/.zshrc
+  echo "source \$HOME/.zsh/key-bindings.zsh" >>~/.zshrc
 
-    echo -e "Shell Configurations"
-    if [[ "$OS" == "Darwin" ]]; then
-        chsh -s /bin/zsh &> /dev/null
-    fi
-    if [[ "$OS" == "Linux" ]]; then
-        sudo usermod -s /usr/bin/zsh $(whoami) &> /dev/null
-        sudo usermod -s /usr/bin/zsh root &> /dev/null
-    fi
-    if mv -n ~/.zshrc ~/.zshrc-backup-$(date +"%Y-%m-%d") &> /dev/null; then
-        echo -e " → Backing up the current .zshrc config to .zshrc-backup-date"
-    fi
-    (cd ~/ && curl -O https://raw.githubusercontent.com/gustavohellwig/gh-zsh/main/.zshrc) &> /dev/null
-    echo "source \$HOME/.zsh/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
-    echo "source \$HOME/.zsh/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh" >> ~/.zshrc
-    echo "source \$HOME/.zsh/completion.zsh" >> ~/.zshrc
-    echo "source \$HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
-    echo "source \$HOME/.zsh/history.zsh" >> ~/.zshrc
-    echo "source \$HOME/.zsh/key-bindings.zsh" >> ~/.zshrc
+  # Theme Installation
 
-    # Theme Installation
+  echo "Theme Installation"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k &>/dev/null
+  (cd ~/ && curl -O https://raw.githubusercontent.com/gustavohellwig/gh-zsh/main/.p10k.zsh) &>/dev/null
 
-    echo "Theme Installation"
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k &> /dev/null
-    (cd ~/ && curl -O https://raw.githubusercontent.com/gustavohellwig/gh-zsh/main/.p10k.zsh) &> /dev/null
+  # Plugins Installations
 
-    # Plugins Installations
+  echo "Plugins Installations"
+  git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ~/.zsh/fast-syntax-highlighting &>/dev/null
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions &>/dev/null
+  (cd ~/.zsh/ && curl -O https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/completion.zsh) &>/dev/null
+  (cd ~/.zsh/ && curl -O https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/history.zsh) &>/dev/null
+  (cd ~/.zsh/ && curl -O https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/key-bindings.zsh) &>/dev/null
 
-    echo "Plugins Installations"
-    git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ~/.zsh/fast-syntax-highlighting &> /dev/null
-    git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions &> /dev/null
-    (cd ~/.zsh/ && curl -O https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/completion.zsh) &> /dev/null
-    (cd ~/.zsh/ && curl -O https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/history.zsh) &> /dev/null
-    (cd ~/.zsh/ && curl -O https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/key-bindings.zsh) &> /dev/null
+  if [[ "$OS" == "Linux" ]]; then
+    sudo cp -r /home/"$(whoami)"/.zshrc /root/
+    sudo cp -r /home/"$(whoami)"/.zsh /root/
+    sudo cp -r /home/"$(whoami)"/.p10k.zsh /root
+  fi
 
-    if [[ "$OS" == "Linux" ]]; then
-      sudo cp -r /home/"$(whoami)"/.zshrc /root/
-      sudo cp -r /home/"$(whoami)"/.zsh /root/
-      sudo cp -r /home/"$(whoami)"/.p10k.zsh /root
-    fi
+  echo -e "Installation Finished"
+  echo -e "You may need to reopen the terminal if the theme doesn't load automatically."
 
-    echo -e "Installation Finished"
-    echo -e "You may need to reopen the terminal if the theme doesn't load automatically."
+  # Ensure the ~/.zsh directory exists
+  mkdir -p $HOME/.zsh
 
+  # Install Plugins: z sudo
+  echo "Installing z plugin..."
+  curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/z/z.plugin.zsh -o $HOME/.zsh/z.plugin.zsh
+  echo "source $HOME/.zsh/z.plugin.zsh" >>$HOME/.zshrc
 
-    # Ensure the ~/.zsh directory exists
-    mkdir -p $HOME/.zsh
+  echo -"Installing sudo plugin..."
+  curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh -o $HOME/.zsh/sudo.plugin.zsh
+  echo "source $HOME/.zsh/sudo.plugin.zsh" >>$HOME/.zshrc
 
-    # Install Plugins: z sudo
-    echo "Installing z plugin..."
-    curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/z/z.plugin.zsh -o $HOME/.zsh/z.plugin.zsh
-    echo "source $HOME/.zsh/z.plugin.zsh" >> $HOME/.zshrc
+  echo "Zsh plugin installation complete!"
 
-    echo -"Installing sudo plugin..."
-    curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh -o $HOME/.zsh/sudo.plugin.zsh
-    echo "source $HOME/.zsh/sudo.plugin.zsh" >> $HOME/.zshrc
+  echo "Setting up personal Functions and Alias"
 
-    echo "Zsh plugin installation complete!"
-
-echo "Setting up personal Functions and Alias"
-
-
-cat <<EOF >> ~/.zshrc
+  cat <<EOF >>~/.zshrc
 
     HIST_STAMPS="%Y-%m-%d %T "
 
@@ -324,7 +335,7 @@ fi
 # Installing fonts
 #--------------------------------------------------
 echo "Installing Font Awesome"
-sudo nala install -y fonts-font-awesome 
+sudo nala install -y fonts-font-awesome
 
 echo "Installing Cascadia Cove Nerd Font and setting up as default font for terminator"
 
@@ -337,10 +348,10 @@ unzip ~/cascadia-code-nerd-font.zip -d ~/.local/share/fonts
 # Remove the downloaded zip file
 rm ~/cascadia-code-nerd-font.zip
 
-#Terminator various configuration 
+#Terminator various configuration
 echo "Installing and Config Terminator Theme"
 mkdir -p ~/.config/terminator/
-cat <<EOF >> ~/.config/terminator/config
+cat <<EOF >>~/.config/terminator/config
 [global_config]
 [keybindings]
 [profiles]
@@ -381,3 +392,4 @@ echo "Reloading Zsh shell to apply changes..."
 exec zsh
 
 echo "Installation and configuration complete!"
+
